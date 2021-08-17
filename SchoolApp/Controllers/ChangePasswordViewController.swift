e42cfdefca4ea05bd5b34400ae02ab82949fa421
niceenergy <ahberk.tas@gmail.com>
2021-08-17 13:28:06 +0300

    Şifre değiştirme ekranı şifreler aynı mı diye bakıyor

--- SchoolApp/Controllers/ChangePasswordViewController.swift
+++ SchoolApp/Controllers/ChangePasswordViewController.swift
@@ -2,7 +2,7 @@
 import UIKit
 
 class ChangePasswordViewController: UIViewController {
-
+    
     @IBOutlet weak var oldPasswordText: UITextField!
     @IBOutlet weak var newPasswordText: UITextField!
     @IBOutlet weak var confirmPasswordText: UITextField!
@@ -16,4 +16,21 @@ class ChangePasswordViewController: UIViewController {
         newPasswordText.addBottomBorder()
         confirmPasswordText.addBottomBorder()
     }
+    func checkPasswords() {
+        if newPasswordText.text == confirmPasswordText.text {
+            newPasswordText.backgroundColor = .green
+            confirmPasswordText.backgroundColor = .green
+        }
+        else{
+            newPasswordText.backgroundColor = .red
+            confirmPasswordText.backgroundColor = .red
+        }
+    }
+    @IBAction func newPasswordChanged(_ sender: Any) {
+        checkPasswords()
+    }
+    @IBAction func confirmPasswordChanged(_ sender: Any) {
+        checkPasswords()
+    }
 }
+
